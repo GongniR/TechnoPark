6d79e45929f6d115126b38ecd1f47c05b3cc2754
GongniR <87619663+GongniR@users.noreply.github.com>
2022-10-29 11:19:11 +0300

    Update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from PyQt5 import QtWidgets, uic
+from PyQt5 import QtWidgets, uic, QtGui
 from PyQt5.QtWidgets import QFileDialog
+from PyQt5.QtGui import QPixmap
 import sys
 import cv2
 import qimage2ndarray
@@ -9,16 +10,21 @@ class Ui(QtWidgets.QMainWindow):
     def __init__(self):
         super(Ui, self).__init__()
         uic.loadUi('main_form.ui',self)
-
+        self.image_path = ''
 #         Кнопки
         self.OpenFile_pushButton.clicked.connect(self.push_btn)
 
     #  Действие для кнопок
     def push_btn(self):
         ofd = QFileDialog()
-        path = ofd.getOpenFileName(ofd)
-        self.PathFile_lineEdit.setText(path[0])
+        self.image_path = ofd.getOpenFileName(ofd)
+        self.PathFile_lineEdit.setText(self.image_path[0])
+        self.view_image(self.image_path[0])
 
+    def view_image(self, image_path):
+        image = cv2.imread(image_path)
+        pixmap = QPixmap(image_path)
+        self.Image_label.setPixmap(pixmap)
 app = QtWidgets.QApplication(sys.argv)
 window = Ui()
 window.show()
